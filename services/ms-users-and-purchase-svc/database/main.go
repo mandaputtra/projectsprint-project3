82b22cdaacfd117898dc21938a8e5fa7e05e9043
Manda Putra <mandaputra8@gmail.com>
2025-01-23 13:36:06 +0700

    feat: database preparations

--- services/ms-users-and-purchase-svc/database/main.go
+++ services/ms-users-and-purchase-svc/database/main.go
@@ -11,16 +11,14 @@ import (
 )
 
 type User struct {
-	ID         string `gorm:"primaryKey"`
-	Email      string `gorm:"unique;not null"`
-	Password   string `gorm:"not null"`
-	Preference string
-	WeightUnit string
-	HeightUnit string
-	Weight     float64
-	Height     float64
-	Name       string
-	ImageUri   string
+	ID                string `gorm:"primaryKey"`
+	Email             string `gorm:"unique"`
+	Phone             string `gorm:"unique"`
+	Password          string `gorm:"not null"`
+	FileId            string
+	BankAccountName   string
+	BankAccountHolder string
+	BankAccountNumber string
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
@@ -28,6 +26,27 @@ func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
 		user.ID = uuid.NewString()
 	}
 	return
+}
+
+type Purchases struct {
+	ID                  string `gorm:"primaryKey"`
+	SenderName          string `gorm:"required"`
+	SenderContactType   string `gorm:"required"` // "email"/"phone"
+	SenderContactDetail string `gorm:"required"`
+	PurchaseProofs      string
+	OrderItems          []PurchaseItems `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:PurchaseID"`
+}
+
+type PurchaseItems struct {
+	PurchaseID string `gorm:"required"`
+	ProductID  string `gorm:"required"`
+	Qty        int64  `gorm:"required"`
+}
+
+type FileID struct {
+	ID               string `gorm:"primaryKey"`
+	FileUri          string `gorm:"required"`
+	FileThumbnailUri string `gorm:"required"`
 }
 
 // Setup database
@@ -57,7 +76,7 @@ func ConnectDatabase(env config.Environment) *gorm.DB {
 	log.Printf("Connection successfull. Result from test SQL: %s\n", result)
 
 	// Migrations
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Purchases{}, &PurchaseItems{})
 	return db
 }
 
